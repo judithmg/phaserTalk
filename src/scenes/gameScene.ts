e5bee04e71c45d587565a1c2443f9c032e9a4ed8
david.carmona_maroto <david.carmona_maroto@contractors.roche.com>
2019-08-19 10:33:15 +0200

    add game over image

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -1,4 +1,6 @@
 function hitBomb() {
+    this.add.image(400, 200, 'gameOver');
+    this.add.image(400, 450, 'pressF5');
     this.physics.pause();
     this.scene.gameOver = true;
 }
@@ -46,6 +48,8 @@ class GameScene extends Phaser.Scene {
         this.load.image('ground', '/assets/platform.png');
         this.load.image('star', '/assets/star.png');
         this.load.image('bomb', '/assets/bomb.png');
+        this.load.image('gameOver', '/assets/gameOver.png');
+        this.load.image('pressF5', '/assets/pressF5.png');
         this.load.spritesheet('dude', '/assets/dude.png', {
             frameWidth: 32,
             frameHeight: 48
