44460141611b33465420a7e10cbdc3f3da72dcfe
diro <diro@erni-espana.es>
2019-09-25 17:18:55 +0200

    Adding privates and types

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -12,12 +12,12 @@ function collectStar(player, star) {
 }
 
 class GameScene extends Phaser.Scene {
-    player: Phaser.Physics.Arcade.Sprite;
-    platforms;
-    cursors: any;
-    stars;
-    score = 0;
-    scoreText;
+    private player: Phaser.Physics.Arcade.Sprite;
+    private platforms: Phaser.Physics.Arcade.StaticGroup;
+	private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    private stars/*: Phaser.Physics.Arcade.Group*/;
+    private score: number = 0;
+    private scoreText: Phaser.GameObjects.Text;
 
     constructor() {
         super({
